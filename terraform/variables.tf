999518d9a8fe86f6cf1aca403f6667df1aced2ed
Ankit Jethi <ankit.jethi47@gmail.com>
2021-10-08 09:17:17 +0000

    Setup terraform config - AWS Provider, S3 Backend.

---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_region" {
+  description = "The AWS region you want to create your infrastructure in."
+  type = string
+  default = "ap-south-1"
+}
---
+++ terraform/versions.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_version = "1.0.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.61.0"
+    }
+  }
+} 
+
