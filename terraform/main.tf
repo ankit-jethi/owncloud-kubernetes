3dbe932d5f96dbd1fc4bf0cdabd9e4eaa847d457
Ankit Jethi <ankit.jethi47@gmail.com>
2021-10-08 12:06:38 +0000

    Setup VPC.

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,3 +7,13 @@ terraform {
     encrypt = true
   }
 }
+
+#---------VPC-----------
+
+resource "aws_vpc" "oc" {
+  cidr_block = var.vpc_cidr_block
+  enable_dns_support = var.vpc_enable_dns_support
+  enable_dns_hostnames = var.vpc_enable_dns_hostnames
+
+  tags = var.vpc_tags
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,23 @@ variable "aws_region" {
   type = string
   default = "ap-south-1"
 }
+
+variable "vpc_cidr_block" {
+  description = "The CIDR block for the VPC."
+  type = string
+}
+
+variable "vpc_enable_dns_support" {
+  description = "Indicates whether the DNS resolution is supported."
+  type = bool
+}
+
+variable "vpc_enable_dns_hostnames" {
+  description = "Indicates whether instances with public IP addresses get corresponding public DNS hostnames."
+  type = bool
+}
+
+variable "vpc_tags" {
+  description = "Tags for your VPC."
+  type = map(string)
+}
